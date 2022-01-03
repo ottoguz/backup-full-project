68bd2d5c84025ac60054d02518fa1a919f197dda
Gustavo Ottoboni <ottoguz87@gmail.com>
2022-01-03 10:08:58 -0300

    debuged log file for windows, removed test files

--- backup_full_otto.py
+++ backup_full_otto.py
@@ -1,17 +1,19 @@
 import subprocess
 import time
+from sys import platform
 
 
 def select_os():
-    op_sys = str(input('Enter the OS(linux/windows):'))
-    if op_sys == 'linux':
-        return op_sys
-    elif op_sys == 'windows':
-        return op_sys
+    if platform == 'linux' or 'linux2':
+        return 'linux'
+    elif platform == 'win32':
+        return 'windows'
+    #elif platform == 'darwin':
+        #return 'OS X'
 
 
-def start(start_time):
-    start_banner = '''
+def header(start_time):
+    start = '''
   ===========================================================================
 ||   ____          _____ _  ___    _ _____    ______ _    _ _      _         ||
 ||  |  _ \   /\   / ____| |/ / |  | |  __ \  |  ____| |  | | |    | |        ||
@@ -27,10 +29,10 @@ def start(start_time):
                  FULL BACKUP FROM THE FILE SERVER BEGAN AT {}s
   ===========================================================================
     '''.format(start_time)
-    return start_banner
+    return start
 
 
-def finish(st_day, st_time, path_log, bkp_name):
+def footer(st_day, st_time, path_log, bkp_name):
     today = time.strftime('%d-%m-%y')
     end_time = time.strftime('%H:%M:%S')
     bkp_name = bkp_name.replace('tar cvf', '')
@@ -46,26 +48,51 @@ def finish(st_day, st_time, path_log, bkp_name):
     return final
 
 
+def unmount_disk(disk):
+    try:
+        umount = 'umount {} /mnt'.format(disk)
+        subprocess.call(umount, shell=True)
+        return True
+    except OSError:
+        return False
+
+
 def generate_log_linux():
-    date = time.strftime('%H:%M:%S')
-    file_log = '{}-backup-full.txt.tar.gz'.format(date)
+    date = time.strftime('%d-%m-%y')
+    file_log = '{}-backup-full.txt'.format(date)
     path_log = '/var/log/backup/backup-full/{}'.format(file_log)
     return path_log
 
 
 def generate_log_windows():
-    date = time.strftime('%d-%m-%y')
+    #date = time.strftime('%y-%m-%d')
     time_bkp = time.strftime('%H:%M:%S')
-    file_log = '{} {}-backup-full.zip'.format(date, time_bkp)
+    file_log = '{}-backup-full.zip'.format(time_bkp)
     path_log = 'E:\\backup\\backup_full_logs {}'.format(file_log)
     return path_log
 
 
+def log_file_windows(bkp_start_time, file_list, final):
+    file = 'backupfull_log_{}.txt' .format(time.strftime('%d-%m-%y'))
+    f = open(file, 'w')
+    f.write(bkp_start_time)
+    f.write(file_list)
+    f.write(final)
+    f.close()
+    return file
+
+
+def gen_list_windows():
+    files = 'cd /d C:\\Users\\55359\\Desktop\\Software_Engineering\\Python 3\\LPA && dir /s /b'
+    files_out = subprocess.getoutput(files)
+    return files_out
+
+
 def gen_backup_linux():
     date = time.strftime('%d-%m-%y')
-    bkp_file_name = '{}-backup-full.tar.gz'.format(date)
-    bkp_destination = '/home/gustavo/backup/{}'.format(bkp_file_name)
-    source_path = '/home/gustavo/bkp_test/'
+    bkp_file_name = '{}_backup-full.tar.gz'.format(date)
+    bkp_destination = '/mnt/backup/{}'.format(bkp_file_name)
+    source_path = '/home/gustavo/Documents/Pycharm/LPA'
     backup = 'tar cvf {} {}'.format(bkp_destination, source_path)
     return backup
 
@@ -73,65 +100,72 @@ def gen_backup_linux():
 def gen_backup_windows():
     date = time.strftime('%d-%m-%y')
     backup_file_name = '{}-backupfull.zip'.format(date)
-    backup_destination = 'E:\\backup\\backup_full'
-    backup_source = 'C:\\files'
-    backup = 'cd /d E:\\backup\\backup_full && tar -cvf {} "{}" '.format(backup_file_name, backup_source)
+    #backup_destination = 'E:\\backup\\backup_full'
+    backup_source = 'C:\\Users\\55359\\Desktop\\Software_Engineering\\Python 3\\LPA'
+    backup = 'cd /d F:\\backup && tar -cf {} "{}" '.format(backup_file_name, backup_source)
     return backup
 
 
-#def format_log_windows(log_path, file_name):
-
-
 def backup_full_linux():
-    disk = '/dev/sdb'
-    start_time_bkp = time.strftime('%H:%M:%S')
-    start_time = start(start_time_bkp)
-    print(start_time)
+    disk = '/dev/sdb1'
+    bkp_start_time = time.strftime('%H:%M:%S')
+    start_time = header(bkp_start_time)
+    # print(start_time)
 
     backup = gen_backup_linux()
 
     path_log = generate_log_linux()
     log = '>> {}'.format(path_log)
 
-    l = open(path_log, 'w')
-    l.write(start_time)
-    l.close()
+    x = open(path_log, 'w')
+    x.write(start_time)
+    x.close()
 
-    mount = 'mount ' + disk + ' /backup'
+    mount = 'mount ' + disk + ' /mnt'
     subprocess.call(mount, shell=True)
 
     subprocess.call(backup + log, shell=True)
 
     start_day = time.strftime('%d-%m-%y')
-    final = finish(start_day, start_time_bkp, path_log, backup, )
-    print(final)
+    final = footer(start_day, bkp_start_time, path_log, backup)
+
+    r = open(path_log, 'r')
+    content = r.readlines()
+    content.append(final)
     r = open(path_log, 'w')
-    r.write(final)
+    r.writelines(content)
     r.close()
+    for i in range(len(content)):
+        print(content[i], end="")
+
+    unmount_disk(disk)
 
 
 def backup_full_windows():
-    start_time_bkp = time.strftime('%H:%M:%S')
-    start_time = start(start_time_bkp)
+    bkp_start_time = time.strftime('%H:%M:%S')
+    start_time = header(bkp_start_time)
     print(start_time)
 
     backup = gen_backup_windows()
-
     path_log = generate_log_windows()
-    log = 'cd /d E:\\backup\\backup_full_logs && echo {}>>backupfull_log.txt'.format(path_log)
+    file_list = gen_list_windows()
+    print(file_list)
 
-    subprocess.call(log, shell=True)
     subprocess.call(backup, shell=True)
 
-
-
     start_day = time.strftime('%d-%m-%y')
-    final = finish(start_day, start_time_bkp, path_log, backup[39:63])
+    final = footer(start_day, bkp_start_time, path_log, backup[27:51])
     print(final)
+
+    log_file_windows(start_time, file_list, final)
+
+    log_to_folder = 'move C:\\Users\\55359\\Desktop\\Software_Engineering\\Atividade_extensionista_1\\backup-full-project-main\\backupfull_log_{}.txt E:\\backup\\backup_full_logs' .format(start_day)
+    subprocess.call(log_to_folder, shell=True)
 
 
 op_sys = select_os()
-if op_sys == 'linux':
-    backup_full_linux()
-else:
+try:
+    if op_sys == 'linux':
+        backup_full_linux()
+except OSError:
     backup_full_windows()
